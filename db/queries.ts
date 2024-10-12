bf8c87ecd2b5df65b754204d9e10562ae3bb1804
Shivalal <shivalalprasad04@gmail.com>
2024-10-12 08:24:58 +0530

    fixed typoo...

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,8 @@
-import { challengeProgress, courses, units, userProgress } from '@/db/schema';
+import { challengeProgress, courses, units, UserProgress } from '@/db/schema';
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
-import { UserProgress } from "./schema";
+// import { UserProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 export const getUserProgress= cache(async()=>{
