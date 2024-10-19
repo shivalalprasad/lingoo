5ad47a0342624a979688555e5a5e5982c45044fe
Shivalal <shivalalprasad04@gmail.com>
2024-10-20 00:51:39 +0530

    shop ✅ stripe 🚫

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -59,6 +59,8 @@
 
 'use server'
 
+// // TODO: move to constants file
+// import { POINTS_TO_REFILL } from '@/app/(main)/shop/items'
 import db from '@/db/drizzle'
 import { getCourseById, getUserProgress } from '@/db/queries'
 import { challengeProgress, challenges, userProgress } from '@/db/schema'
@@ -66,6 +68,9 @@ import { auth, currentUser } from '@clerk/nextjs'
 import { and, eq } from 'drizzle-orm'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+
+// TODO : move alongside item component into a common file
+const POINTS_TO_REFILL = 10;
 
 export const upsertUserProgress = async (courseId: number) => {
   const { userId } = await auth()
@@ -166,4 +171,24 @@ export const reduceHearts = async (challengeId: number) => {
   revalidatePath('/quests')
   revalidatePath('/leaderboard')
   revalidatePath(`/learn/${lessonId}`)
+}
+
+export const refillHearts = async ()=>{
+  const currentUserProgress = await getUserProgress();
+
+  if(!currentUserProgress) throw new Error ("USer Progress not found");
+
+  if(currentUserProgress.hearts === 5)throw new Error ("Hearts are already full");
+
+  if(currentUserProgress.points < POINTS_TO_REFILL)throw new Error ("Not enough points");
+
+  await db.update(userProgress).set({
+    hearts:5,
+    points:currentUserProgress.points -POINTS_TO_REFILL,
+  }).where(eq(userProgress.userId,currentUserProgress.userId));
+
+  revalidatePath('/shop');
+  revalidatePath('/learn');
+  revalidatePath('/quests');
+  revalidatePath('/leaderboard');
 }
