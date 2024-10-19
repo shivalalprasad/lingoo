2df7ec5f4fa12edabb03845230f93f2f248c9e20
Shivalal <shivalalprasad04@gmail.com>
2024-10-19 11:08:21 +0530

    Challenge Cards ✅

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,8 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./QuestionBubble";
+import { Challenge } from "./challenge";
 
 type Props = {
   initialLessonId: number;
@@ -24,19 +26,55 @@ const Quiz = ({
 }: Props) => {
   const [hearts, SetHearts] = useState(initialHearts);
   const [percentage, SetPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
   return (
-    <Header
-      hearts={hearts}
-      percentage={percentage}
-      hasActiveSubscription={!!userSubscription?.isActive}
-    />
-    // <div>
-    //   <p> initialLessonId:{initialLessonId}</p>
-    //   {/* <p>{initialLessonChallenges}</p> */}
-    //   <p> initialHearts :{initialHearts}</p>
-    //   <p> initialPercentage:{initialPercentage}</p>
-    //   <p>userSubscription:{userSubscription}</p>
-    // </div>
+    <>
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                // onSelect={onSelect}
+                onSelect={() => {}}
+                // status={status}
+                status="none"
+                // selectedOption={selectedOption}
+                selectedOption={undefined}
+                // disabled={pending}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
