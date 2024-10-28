d9441cc5a7d72ceb51fee9f8da877a70d298f77b
Shivalal <shivalalprasad04@gmail.com>
2024-10-28 21:27:07 +0530

    leaderboars and learn pages changes

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -48,9 +48,9 @@ export default async function LearnPage() {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={isPro}
         />
         {!isPro && <Promo />}
